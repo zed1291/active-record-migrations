f0a8d4bd734c356e3e9fb06e00c838078b0441ec
“zed” <zed@kolk.me>
2021-01-27 19:02:54 -0600

    commit tree a2c6331593e5e7efbc5e1c4b1ab4ff9ec1c94cde

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,13 @@ require 'yaml/store'
 require 'ostruct'
 require 'date'
 
-
 require 'bundler/setup'
 Bundler.require
 
-
 # put the code to connect to the database here
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
 
-
-require_relative "../artist.rb"
+require_relative "../artist.rb" 
